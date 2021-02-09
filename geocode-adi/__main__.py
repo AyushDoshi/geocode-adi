9a8dcbca5a6ed9c19a3fb9cb3376837b4b07e69b
Ayush Doshi <ayushdoshi1@gmail.com>
2021-02-09 02:34:47 -0500

    Update __main__.py

--- geocode-adi/__main__.py
+++ geocode-adi/__main__.py
@@ -30,32 +30,34 @@ def file_path(path):
     if os.path.isfile(path):
         return path
     else:
-        raise argparse.ArgumentTypeError(
-            f"Address File: Given file, {path}, does not exist."
-        )
+        raise argparse.ArgumentTypeError(f"Address File: Given file, {path}, does not exist.")
 
 
-parser = argparse.ArgumentParser(
-    description="Convert Addresses to Area Deprivation Indices."
-)
-parser.add_argument(
-    "addresses_file",
-    metavar="address_file",
-    type=file_path,
-    help="Path to file that contains at least 4 columns, 'Address', 'City', 'State', 'ZIP Code', "
-    "and must be labeled as such. 'Address' must contain at least the street number and street "
-    "name; other information, such as apartment/suite number, are not necessary but tolerated.",
-)
-parser.add_argument(
-    "-sf",
-    "--skip_first_pass",
-    dest="skip_first",
-    default=False,
-    action="store_true",
-    help="Skip the first pass conversion of addresses to U.S. Census Block Groups using the U.S. "
-    "Census Geocoder. Go straight to addresses to coordinates conversion.",
-)
-args = parser.parse_args()
+def main():
+    parser = argparse.ArgumentParser(
+        description="Convert Addresses to Area Deprivation Indices."
+    )
+    parser.add_argument(
+        "addresses_file",
+        metavar="address_file",
+        type=file_path,
+        help="Path to file that contains at least 4 columns, 'Address', 'City', 'State', 'ZIP Code', "
+             "and must be labeled as such. 'Address' must contain at least the street number and street "
+             "name; other information, such as apartment/suite number, are not necessary but tolerated.",
+    )
+    parser.add_argument(
+        "-sf",
+        "--skip_first_pass",
+        dest="skip_first",
+        default=False,
+        action="store_true",
+        help="Skip the first pass conversion of addresses to U.S. Census Block Groups using the U.S. "
+             "Census Geocoder. Go straight to addresses to coordinates conversion.",
+    )
+    args = parser.parse_args()
+
+    addresses_to_adi(args.addresses_file, not args.skip_first)
+
 
 if __name__ == "__main__":
-    addresses_to_adi(args.addresses_file, not args.skip_first)
+    main()
